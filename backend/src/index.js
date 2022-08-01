05c19ebb33c64236adaa42cb412970c4270db177
cmd-mish <popov.mikhail.al@gmail.com>
2022-08-01 15:57:08 +0300

    Initial commit - simple node application

---
+++ backend/src/index.js
@@ -0,0 +1,21 @@
+require('dotenv').config()
+const express = require('express')
+const csv = require('csvtojson')
+
+const app = express()
+const PORT = 3000
+
+
+const csvFile = './data/stations.csv'
+
+app.get('/api/stations', (req, res) => {
+  csv()
+    .fromFile(csvFile)
+    .then((obj) => {
+      return res.json(obj)
+    })
+})
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`)
+})
