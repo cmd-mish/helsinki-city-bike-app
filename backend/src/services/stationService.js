cd6e2b650c0f8829ba27d4115a6e77b0deda6818
cmd-mish <popov.mikhail.al@gmail.com>
2022-08-02 16:23:26 +0300

    added api endpoint for a single station

--- backend/src/services/stationService.js
+++ backend/src/services/stationService.js
@@ -2,8 +2,19 @@ const stationService = require('express').Router()
 const Station = require('../models/station')
 
 stationService.get('/', async (req, res) => {
-  const services = await Station.find({})
-  res.json(services)
+  const stations = await Station.find({})
+  return res.json(stations)
+})
+
+stationService.get('/:id', async (req, res) => {
+  try {
+    const station = await Station.findById(req.params.id)
+    if (station) {
+      return res.json(station)
+    }
+  } catch (e) {
+    return res.status(400).json({ error: e.message })
+  }
 })
 
 module.exports = stationService
