69cc7c89a039692a472a40dbcb4b21fd1a696e84
cmd-mish <popov.mikhail.al@gmail.com>
2022-08-03 00:02:38 +0300

    combined start & end journey endpoints

--- backend/src/services/stationService.js
+++ backend/src/services/stationService.js
@@ -11,33 +11,9 @@ stationService.get('/:id', async (req, res) => {
   try {
     const station = await Station.findById(req.params.id)
     if (station) {
-      return res.json(station)
-    }
-    return res.status(404).json({ error: 'station not found' })
-  } catch (e) {
-    return res.status(400).json({ error: e.message })
-  }
-})
-
-stationService.get('/starting/:id', async (req, res) => {
-  const staitonID = req.params.id
-  try {
-    const result = await Journey.countDocuments({ departure_station_id: staitonID })
-    if (result) {
-      return res.json({ id: staitonID, startJourneyCount: result })
-    }
-    return res.status(404).json({ error: 'station not found' })
-  } catch (e) {
-    return res.status(400).json({ error: e.message })
-  }
-})
-
-stationService.get('/ending/:id', async (req, res) => {
-  const staitonID = req.params.id
-  try {
-    const result = await Journey.countDocuments({ return_station_id: staitonID })
-    if (result) {
-      return res.json({ id: staitonID, endJourneyCount: result })
+      const startJourneyCount = await Journey.countDocuments({ departure_station_id: station.ID })
+      const endJourneyCount = await Journey.countDocuments({ return_station_id: station.ID })
+      return res.json({ ...station._doc, startJourneyCount, endJourneyCount })
     }
     return res.status(404).json({ error: 'station not found' })
   } catch (e) {
