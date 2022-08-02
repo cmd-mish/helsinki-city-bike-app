84a0b2784e5793a9624cea60f2461aa1951cb652
cmd-mish <popov.mikhail.al@gmail.com>
2022-08-02 14:45:03 +0300

    simple search in frontend Stations component

--- frontend/src/components/Stations.js
+++ frontend/src/components/Stations.js
@@ -3,6 +3,7 @@ import stationService from '../services/stations'
 
 const Stations = () => {
   const [stations, setStations] = useState([])
+  const [search, setSearch] = useState('')
 
   useEffect(() => {
     stationService
@@ -10,10 +11,15 @@ const Stations = () => {
       .then(response => setStations(response))
   }, [])
 
-  console.log(stations)
+  if (stations.length < 1) return <div>loading...</div>
+
   return (
     <div>
       <h2>Stations</h2>
+      <div>
+        filter stations by name
+        <input type='text' onChange={(event) => setSearch(event.target.value)} />
+      </div>
       <table>
         <tbody>
           <tr>
@@ -25,12 +31,13 @@ const Stations = () => {
             <th>Coordinates</th>
           </tr>
           {stations
+            .filter(station => station.Name.toLowerCase().includes(search.toLowerCase()))
             .map(station => {
               return (
                 <tr key={station._id}>
                   <td>{station.FID}</td>
                   <td>{station.Name}</td>
-                  <td>{station.Osoite}, {station.Kaupunki}</td>
+                  <td>{station.Osoite}{station.Kaupunki ? `, ${station.Kaupunki}` : ''} </td>
                   <td>{station.Operaattor}</td>
                   <td>{station.Kapasiteet}</td>
                   <td>x: {station.x}, y: {station.y}</td>
