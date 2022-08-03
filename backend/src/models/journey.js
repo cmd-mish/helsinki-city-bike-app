8de751cdee1137ccd3006a03ff23233973fe7478
cmd-mish <popov.mikhail.al@gmail.com>
2022-08-03 20:30:41 +0300

    added db indexing to schema

--- backend/src/models/journey.js
+++ backend/src/models/journey.js
@@ -4,34 +4,42 @@ const schema = new mongoose.Schema({
   departure: {
     type: Date,
     required: true,
+    index: true,
   },
   return: {
     type: Date,
     required: true,
+    index: true,
   },
   departure_station_id: {
     type: Number,
     required: true,
+    index: true,
   },
   departure_station_name: {
     type: String,
-    required: false
+    required: false,
+    index: true,
   },
   return_station_id: {
     type: Number,
     required: true,
+    index: true,
   },
   return_station_name: {
     type: String,
-    required: false
+    required: false,
+    index: true,
   },
   covered_distance: {
     type: Number,
     required: true,
+    index: true,
   },
   duration: {
     type: Number,
     required: true,
+    index: true,
   }
 })
 
