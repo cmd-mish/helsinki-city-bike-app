91fa892f97ec46b9787c8e19c928f5f33ad85933
cmd-mish <popov.mikhail.al@gmail.com>
2022-08-04 00:55:38 +0300

    improved paginator

--- frontend/src/components/Paginator.js
+++ frontend/src/components/Paginator.js
@@ -3,11 +3,30 @@ import Button from 'react-bootstrap/Button'
 const Paginator = ({ changePage, currentPage, totalPages }) => {
   return (
     <div className="text-center mt-2 mb-4">
-      <Button variant="outline-dark" size="sm" onClick={() => changePage('first')}>first page</Button>&nbsp;
-      <Button variant="outline-dark" size="sm" onClick={() => changePage(-1)}>previous page</Button>
+      {currentPage === 1 ?
+        <span>
+          <Button variant="outline-dark" size="sm" disabled>first page</Button> &nbsp;
+          <Button variant="outline-dark" size="sm" disabled>previous page</Button>
+        </span>
+        :
+        <span>
+          <Button variant="outline-dark" size="sm" onClick={() => changePage('first')}>first page</Button>&nbsp;
+          <Button variant="outline-dark" size="sm" onClick={() => changePage(-1)}>previous page</Button>
+        </span>
+      }
       &nbsp;you are on page {currentPage} / {totalPages}&nbsp;
-      <Button variant="outline-dark" size="sm" onClick={() => changePage(1)}>next page</Button>&nbsp;
-      <Button variant="outline-dark" size="sm" onClick={() => changePage('last')}>last page</Button>
+
+      {currentPage === totalPages ?
+        <span>
+          <Button variant="outline-dark" size="sm" disabled>next page</Button>&nbsp;
+          <Button variant="outline-dark" size="sm" disabled>last page</Button>
+        </span>
+        :
+        <span>
+          <Button variant="outline-dark" size="sm" onClick={() => changePage(1)}>next page</Button>&nbsp;
+          <Button variant="outline-dark" size="sm" onClick={() => changePage('last')}>last page</Button>
+        </span>
+      }
     </div>
   )
 }
