6b52021d1f98bf93c260ccdbda4a424c9fcf383d
cmd-mish <popov.mikhail.al@gmail.com>
2022-08-05 00:34:50 +0300

    responsive paginator

--- frontend/src/components/Paginator.js
+++ frontend/src/components/Paginator.js
@@ -2,31 +2,39 @@ import Button from 'react-bootstrap/Button'
 
 const Paginator = ({ changePage, currentPage, totalPages }) => {
   return (
-    <div className="text-center mt-2 mb-4">
-      {currentPage === 1 ?
-        <span>
-          <Button variant="outline-dark" size="sm" disabled>first page</Button> &nbsp;
-          <Button variant="outline-dark" size="sm" disabled>previous page</Button>
-        </span>
-        :
-        <span>
-          <Button variant="outline-dark" size="sm" onClick={() => changePage('first')}>first page</Button>&nbsp;
-          <Button variant="outline-dark" size="sm" onClick={() => changePage(-1)}>previous page</Button>
-        </span>
-      }
-      &nbsp;you are on page {currentPage} / {totalPages}&nbsp;
+    <div className="container text-center mt-2 mb-4">
+      <div class="row">
+        <div class="col">
+          {currentPage === 1 ?
+            <span>
+              <Button variant="outline-dark" size="sm" disabled>first page</Button> &nbsp;
+              <Button variant="outline-dark" size="sm" disabled>previous page</Button>
+            </span>
+            :
+            <span>
+              <Button variant="outline-dark" size="sm" onClick={() => changePage('first')}>first page</Button>&nbsp;
+              <Button variant="outline-dark" size="sm" onClick={() => changePage(-1)}>previous page</Button>
+            </span>
+          }
+        </div>
+        <div class="col">
+          {currentPage} / {totalPages}
+        </div>
 
-      {currentPage === totalPages ?
-        <span>
-          <Button variant="outline-dark" size="sm" disabled>next page</Button>&nbsp;
-          <Button variant="outline-dark" size="sm" disabled>last page</Button>
-        </span>
-        :
-        <span>
-          <Button variant="outline-dark" size="sm" onClick={() => changePage(1)}>next page</Button>&nbsp;
-          <Button variant="outline-dark" size="sm" onClick={() => changePage('last')}>last page</Button>
-        </span>
-      }
+        <div class="col">
+          {currentPage === totalPages ?
+            <span>
+              <Button variant="outline-dark" size="sm" disabled>next page</Button>&nbsp;
+              <Button variant="outline-dark" size="sm" disabled>last page</Button>
+            </span>
+            :
+            <span>
+              <Button variant="outline-dark" size="sm" onClick={() => changePage(1)}>next page</Button>&nbsp;
+              <Button variant="outline-dark" size="sm" onClick={() => changePage('last')}>last page</Button>
+            </span>
+          }
+        </div>
+      </div>
     </div>
   )
 }
